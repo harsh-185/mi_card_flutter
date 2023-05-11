9cc051c2b9e6ca31eaea8fb5606841c1af8f6d77
harsh-185 <harsh.maheshwari180504@gmail.com>
2023-05-12 00:19:05 +0530

    comit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,94 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp()
   );
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal[700],
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 80,
+                backgroundImage: AssetImage('images/pro.jpg')
+                ),
+              Text(
+                'Harsh Maheshwari',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Kalam'
+                ),
+              ),
+              Text(
+                "STUDENT",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal[100],
+                  letterSpacing: 2,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 250,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal[900],
+                  ),
+                  title: Text(
+                    '+1 814-280-(7099)',
+                      style: TextStyle(
+                          color: Colors.teal[900],
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20
+                      ),
+                    ),
+                  ),
+                ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.teal[900],
+                  ),
+                  title: Text(
+                    'hjm5556@psu.edu',
+                    style: TextStyle(
+                        color: Colors.teal[900],
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20
+                    ),
+                  ),
+                ),
+              ),
+            ],
+            )
+          ),
+        ),
+      );
+  }
 }
